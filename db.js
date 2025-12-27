074310902ef26edc17a148cf06c88a0802da48b6
TanayNexintel2025 <tanay.g@nexintels.com>
2025-12-27 19:20:17 +0530

    Stabilize Railway connection pool

--- db.js
+++ db.js
@@ -1,10 +1,39 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root1234',
-  database: 'mini_backend'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
 });
 
 module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql = require('mysql2');
+
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root1234',
+//   database: 'mini_backend'
+// });
+
+// module.exports = db;
